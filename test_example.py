10d47ec87ee4a9e6adec17a3afd8a3accb98af6c
LucB127 <luc.steinbuch@wur.nl>
2023-06-22 12:22:59 +0200

    test die moet falen toegevoegd

--- test_example.py
+++ test_example.py
@@ -1,5 +1,10 @@
 from example import add
+from example import subtract
 
 
 def test_add_positive():
         assert add(1,1) > 0
+        
+        
+def test_subtract():
+        assert subtract(1,1) == 0        
